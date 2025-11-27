f517580f07651ecc92421d00b0e439ae6317882d
Ben Hall <benji2357@gmail.com>
2025-11-27 06:38:12 +0000

    fix(Layoffs): better notes

--- src/components/Layoffs.tsx
+++ src/components/Layoffs.tsx
@@ -21,18 +21,21 @@ export default function Layoffs() {
           <ul>
             <li>
               This is OSINT and is likely a significant underestimate, although
-              it can give insight into trends.
+              it can give insight into trends
             </li>
             <li>
               Latest data is not complete yet so the last period is likely
               understated (except the trueup annual 2025 data which is a
-              projection).
+              projection)
             </li>
             <li>
               Trends in the monthly chart are computed with exponential moving
-              averages. Where trueup data is available it is normalized to match
-              the layoffs.fyi data within the overlapping period before
-              calculating the trend.
+              averages
+            </li>
+            <li>
+              Where trueup data is available it is normalized to match the
+              layoffs.fyi data within the overlapping period before calculating
+              the trend
             </li>
           </ul>
         </details>
