69a5a452ee25b60288e1e8432858d1692848ebe5
Ben Hall <benji2357@gmail.com>
2025-12-01 21:44:55 +0000

    feat(Notes): add link to GitHub

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -4,6 +4,14 @@ export default function Notes() {
       <h2>Notes</h2>
       <ul>
         <li>
+          <a
+            href="https://github.com/benji6/tech-job-market-dashboard"
+            target="_blank"
+          >
+            Code open source on GitHub
+          </a>
+        </li>
+        <li>
           A lot of this data is OSINT and carries all the typical caveats,
           although it can give insight into trends
         </li>
