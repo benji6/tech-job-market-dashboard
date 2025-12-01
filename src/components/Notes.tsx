f5a9816515e99615fd7b87bb1893d5371db024f1
Ben Hall <benji2357@gmail.com>
2025-12-01 21:23:46 +0000

    fix(Notes): update

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -4,16 +4,12 @@ export default function Notes() {
       <h2>Notes</h2>
       <ul>
         <li>
-          This is OSINT and is likely a significant underestimate, although it
-          can give insight into trends
+          A lot of this data is OSINT and carries all the typical caveats,
+          although it can give insight into trends
         </li>
         <li>
-          Latest data is not complete yet so the last period is likely
+          Latest layoff data is not complete yet so the last period is likely
           understated (except the trueup annual 2025 data which is a projection)
-        </li>
-        <li>
-          Trends in the monthly chart are computed with exponential moving
-          averages
         </li>
         <li>
           Where trueup data is available it is normalized to match the
